6e5eb8920477d25fa1ebf86ef13cae877f89168a
catcoin10 <numberfile256@gmail.com>
2020-03-14 09:04:41 -0400

    This is a more accurate program
    We are using this modification because it helps the program run more fluidly.
    
    Output is in the format of hash, strength, n_hashes

--- code.py
+++ code.py
@@ -8,13 +8,11 @@ def run(n):
   input = 'c79855a0f5344b655b8bc30220b545e2ef25b88b723745b7fed976d46e260c91' # hash of https://is.gd/Bartha
   result = 0
   count = 0
-  x = int(mpmath.factorial(n))
-  print(x)
+  x = int(2**n)
   while result < x:
-    data = str(secrets.randbits(len(str(x))*4)) + " " + input
+    data = str(secrets.randbits(257)) + " " + input
     result = divide(sha256(data.encode('utf-8')).hexdigest())
     count += 1
-    if (count % 100000) == 0:  print(count)
-  return [data, int(result)]
+  return [data, int(result), count]
 
-print(run(9))
+print(run(22))
