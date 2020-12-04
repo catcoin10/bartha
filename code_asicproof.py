6e46ace504739fb50070e9822089918c46ce4111
catcoin10 <numberfile256@gmail.com>
2020-12-04 13:59:22 -0500

    Update code_asicproof.py

--- code_asicproof.py
+++ code_asicproof.py
@@ -12,6 +12,6 @@ def run(x):
     result = divide(binascii.hexlify(argon2.argon2_hash(input,salt)))
     data = binascii.hexlify(argon2.argon2_hash(input,salt))
     count += 1
-  return [str(data)[2:-1], int(result), count]
+  return [str(data)[2:-1], int(salt), count]
 
 print(run(int(sys.argv[1])))
